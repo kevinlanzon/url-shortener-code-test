0b9e74692474c0da6e985944b5453cffcd6cdeb3
Kevin Lanzon <kevinlanzon@gmail.com>
2016-03-21 12:10:08 +0000

    GET route displays homepage content

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -3,9 +3,14 @@ require './app/server'
 describe "My Sinatra Application" do
 
    describe "#GET '/'" do
-    it "loads homepage" do
+    it "loads the homepage" do
       get '/'
       expect(last_response).to be_ok
+    end
+
+    it "displays homepage content" do
+      get '/'
+      expect(last_response.body).to include("URL Shortener")
     end
   end
 end
